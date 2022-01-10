5bcf331e215212ff5c7756ddc5968cebd5054385
Oliver Otzen <oliver.otzen@maibornwolff.de>
2022-01-10 15:07:44 +0100

    code cleanup and refactoring

--- src/main/java/org/maibornwolff/validation/Validation.java
+++ src/main/java/org/maibornwolff/validation/Validation.java
@@ -36,65 +36,75 @@ public class Validation {
         return validation;
     }
 
-    public static <T> Validation validateNotEmpty(String checkee, String message) {
-        return Objects.isNull(checkee) || checkee.isEmpty() ? error(message) : ok();
-    }
-
-    public static <T> Validation validateNotEmpty(Collection<T> checkee, String message) {
-        return Objects.isNull(checkee) || checkee.isEmpty() ? error(message) : ok();
-    }
-
-    public static <T> Validation validateNotEmpty(Collection<T> collection, Function<T, Validation> callable, String message) {
-        if (Objects.isNull(collection) || collection.isEmpty()) {
-            return error(message);
-        }
-        return Validation.of(collection.stream().map(callable).collect(toList()));
-    }
-
     public static <T> Validation validateNotNull(T checkee, String name) {
-        return Objects.isNull(checkee) ? error(name + " should not be null") : ok();
+        return Objects.isNull(checkee) ?
+                error(name + " should not be null") :
+                ok();
     }
 
     public static <T> Validation validateNotNull(T checkee, Function<T, Validation> callable, String message) {
-        if (Objects.isNull(checkee)) {
-            return error(message);
-        }
-        return Validation.of(callable.apply(checkee));
+        return Objects.isNull(checkee) ?
+                error(message) :
+                Validation.of(callable.apply(checkee));
+    }
+
+    public static <T> Validation validateNotEmpty(String checkee, String message) {
+        return Objects.isNull(checkee) || checkee.isEmpty() ?
+                error(message) :
+                ok();
+    }
+
+    public static <T> Validation validateNotEmpty(Collection<T> checkee, String message) {
+        return Objects.isNull(checkee) || checkee.isEmpty() ?
+                error(message) :
+                ok();
+    }
+
+    public static <T> Validation validateNotEmpty(Collection<T> collection, Function<T, Validation> callable, String message) {
+        return Objects.isNull(collection) || collection.isEmpty() ?
+                error(message) :
+                Validation.of(collection.stream().map(callable).collect(toList()));
+    }
+
+    public static <T> Validation validateNotNull(Collection<T> collection, Function<T, Validation> callable, String message) {
+        return Objects.isNull(collection) ?
+                error(message) :
+                Validation.of(collection.stream().map(callable).collect(toList()));
     }
 
     public static Validation validateNotNullOrEmpty(String checkee, String name) {
-        return checkee == null || checkee.isEmpty() ? error(name + " should have a value") : ok();
-    }
-
-    public static Validation validateNotNullOrEmpty(List<String> checkee, String name) {
-        return checkee == null || checkee.isEmpty() ? error(name + " does not exist or is not populated") : ok();
+        return checkee == null || checkee.isEmpty() ?
+                error(name + " should have a value") :
+                ok();
     }
 
     public static Validation validateNotNullAndMatches(String checkee, String regex, String name) {
-        return checkee == null || !matches(regex, checkee) ? error(name + " should match " + regex) : ok();
+        return checkee == null || !matches(regex, checkee) ?
+                error(name + " should match " + regex) :
+                ok();
     }
 
     public static Validation validateEmptyOrMatches(String checkee, String regex, String name) {
-        return checkee == null || checkee.isEmpty() || matches(regex, checkee) ? ok() : error(name + " should match " + regex);
+        return checkee == null || checkee.isEmpty() || matches(regex, checkee) ?
+                ok() :
+                error(name + " should match " + regex);
     }
 
     public static Validation validateEmptyOrMatches(Optional<String> checkee, String regex, String name) {
-        return checkee.isEmpty() || matches(regex, checkee.get()) ? ok() : error(name + " should match " + regex);
+        return checkee.isEmpty() || matches(regex, checkee.get()) ?
+                ok() :
+                error(name + " should match " + regex);
     }
 
     public static <T> Validation validateIsPresent(Optional<T> checkee, String name) {
-        return validateNotNull(checkee, opt -> opt.isPresent() ? ok() : error(name + " should be present"), name + " should not be null");
+        return validateNotNull(checkee, opt -> opt.isPresent() ?
+                ok() :
+                error(name + " should be present"), name + " should not be null");
     }
 
-    public static <T> Validation validateIsPresent(Optional<T> checkee,  Function<T, Validation> callable, String name) {
-        return checkee.map(t -> Validation.of(callable.apply(t))).orElseGet(() -> error(name + " should be present"));
-    }
-
-    public static <T> Validation validateNonNullCollection(Collection<T> collection, Function<T, Validation> callable, String message) {
-        if (Objects.isNull(collection)) {
-            return error(message);
-        }
-        return Validation.of(collection.stream().map(callable).collect(toList()));
+    public static <T> Validation validateIsPresent(Optional<T> checkee, Function<T, Validation> callable, String name) {
+        return checkee.map(t -> Validation.of(callable.apply(t)))
+                .orElseGet(() -> error(name + " should be present"));
     }
 
     public List<String> getErrors() {
